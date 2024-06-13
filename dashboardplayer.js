38f1e50b5e763c90af67571f19886b333126cf3f
DerickNorlan <148537104+Dericknorlan@users.noreply.github.com>
2024-06-13 23:05:39 +0800

    Add files via upload

---
+++ dashboardplayer.js
@@ -0,0 +1,53 @@
+$(document).ready(async function() {
+    $(".sidebar").load("sidebarplayer.html", function() {
+        const toggleBtn = $("#toggle-btn");
+        const logo = $(".logo_details .logo").eq(1); // Select the second logo
+        toggleBtn.on("click", function() {
+            $(".sidebar").toggleClass("open");
+            updateLogo();
+            menuBtnChange();
+        });
+        function menuBtnChange() {
+            if (sidebar.hasClass("open")) {
+                logo.hide();
+            } else {
+                logo.show();
+            }
+        }
+    });
+
+    $(document).on('click', function (e) {
+        if (!$(e.target).closest('.sidebar, #toggle-btn').length) {
+          closeSidebar();
+        }
+    });
+
+    function closeSidebar() {
+        $('.sidebar').removeClass('open');
+        updateLogo();
+        // $('.main-content').removeClass('shift');
+    }
+
+    const openBtn = $(".game-card");
+    const closeBtn = $("#closePopup");
+    const modal = $(".gamedetails");
+
+    openBtn.click(function() {
+        modal.addClass("open");
+    });
+
+    closeBtn.click(function() {
+        modal.removeClass("open");
+    });
+
+    $(".game-card").click(function() {
+        $(".gamedetails").show();
+        $(".modal-overlay").show();
+    });
+
+    $("#closePopup").click(function() {
+        $(".gamedetails").hide();
+        $(".modal-overlay").hide();
+    });
+
+});
